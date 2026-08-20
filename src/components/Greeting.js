8433cd163665b0f2e96713bf009dad33eedc5891
Dmytro Yevich <yevich.dmytro@gmail.com>
2026-08-20 21:47:44 +0300

    Add dummy button to change paragraph test;  Simulate user event to test changes

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import {useState} from "react";
+
 const Greeting = () => {
+    const [isTextChanged, setIsTextChanged] = useState(false);
+
+    const handleChangeTextClick = () => {
+        setIsTextChanged(prev => !prev);
+    }
+
     return (
         <div>
             <h2>Hello World!</h2>
-            <p>It's good to see you!</p>
+            {!isTextChanged && <p>It's good to see you!</p>}
+            {isTextChanged && <p>Text Changed!</p>}
+            <button onClick={handleChangeTextClick}>Change Text</button>
         </div>
     );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,8 +1,9 @@
 import Greeting from "./Greeting";
 import {render, screen} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe('Greeting Component', () => {
-    test('Greeting h2 is present', () => {
+    test('renders h2 with the "Hello World" text', () => {
         // Arrange
         render(<Greeting/>);
 
@@ -13,10 +14,31 @@ describe('Greeting Component', () => {
         const helloWorldHeader = screen.getByText('Hello World', {exact: false});
         expect(helloWorldHeader).toBeInTheDocument();
         expect(helloWorldHeader.tagName).toBe('H2');
+    });
+
+    test('renders "It\'s good to see you!" in a paragraph when text is NOT changed by button', () => {
+        render(<Greeting/>);
 
         const greetingParagraph = document.querySelector('p');
         expect(greetingParagraph).toBeInTheDocument();
-        expect(greetingParagraph.textContent).toBe("It's good to see you!");
+        expect(greetingParagraph.textContent).toBe('It\'s good to see you!');
+
+        const textChangedParagraph = screen.queryByText('Text Changed!');
+        expect(textChangedParagraph).toBeNull();
+    });
+
+    test('renders "Text Changed!" in a paragraph when text is changed by button', async () => {
+        render(<Greeting/>);
+
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        const textChangedParagraph = document.querySelector('p');
+        expect(textChangedParagraph).toBeInTheDocument();
+        expect(textChangedParagraph.textContent).toBe('Text Changed!');
+
+        const greetingParagraph = screen.queryByText('It\'s good to see you!');
+        expect(greetingParagraph).toBeNull();
     });
 });
 
